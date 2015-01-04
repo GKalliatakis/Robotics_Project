2a340eb75aa5c54af56e3c4016f58c61a3f203f9
Gregory Kalliatakis <gkalliatakis@yahoo.gr>
2015-01-04 19:39:37 +0100

    aaaa
    Robotics Project Files

---
+++ humanDetector/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/mscv5/groovy_workspace/sandbox/humanDetector/lib/libgtest.pdb"
+  "/home/mscv5/groovy_workspace/sandbox/humanDetector/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
